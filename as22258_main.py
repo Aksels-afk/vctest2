ababf678817971eb28b6d4a2d68f970884e697f4
Aksels Salavs <akselssalavs@gmail.com>
2023-12-20 23:18:34 +0200

    add plusx()

--- as22258_main.py
+++ as22258_main.py
@@ -1,7 +1,7 @@
 # This is a simple calculation program
 # created to demonstrate version control.
-from as22258-libadd import *
-from as22258-libmult import *
+from as22258_libadd import *
+from as22258_libmult import *
 a = 13
 b = 5
 print(plus(a,b))
