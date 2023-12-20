9f11a61a83ac3ad8c2d83bbc1c3a9371d5654667
Aksels Salavs <akselssalavs@gmail.com>
2023-12-20 22:41:39 +0200

    Initial commit!

---
+++ as22258_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from as22258-libadd import *
+from as22258-libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
