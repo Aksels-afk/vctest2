42e9da884a8ca17e06625d8c0204188683d8f104
Aksels Salavs <akselssalavs@gmail.com>
2023-12-20 23:34:43 +0200

    add multx()

--- as22258_main.py
+++ as22258_main.py
@@ -1,8 +1,9 @@
 # This is a simple calculation program
 # created to demonstrate version control.
-from as22258-libadd import *
-from as22258-libmult import *
+from as22258_libadd import *
+from as22258_libmult import *
 a = 13
 b = 5
 print(plus(a,b))
 print(mult(a,b))
+print(multx(a,b,2))
